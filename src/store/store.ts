912a9364f42e39866608757e458e9b7c35162129
Rennan Silva <jrennan14@gmail.com>
2023-01-26 17:04:13 -0300

    deleting store

--- src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore, Store } from "redux";
-
-import { State } from "./state";
-import { reducer } from "./reducer";
-
-export const store: Store<State> = createStore(reducer);
